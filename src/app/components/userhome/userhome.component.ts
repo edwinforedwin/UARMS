bf5d0925ab44cc17066ed0fa2a43fcca07f38c8b
edwinforedwin <edwinforedwin@gmail.com>
2024-12-20 19:04:55 +0530

    submission changes

--- src/app/components/userhome/userhome.component.ts
+++ src/app/components/userhome/userhome.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-userhome',
-    imports: [],
+    imports: [RouterOutlet],
     templateUrl: './userhome.component.html',
     styleUrl: './userhome.component.css'
 })
